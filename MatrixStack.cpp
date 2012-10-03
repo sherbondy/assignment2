ee84956f2e49d51646f0b4280a601a63dda489f3
Ethan Sherbondy <sherbondye@gmail.com>
2012-10-02 23:22:29 -0400

    oh man, my stack multiplication was backwards.

--- MatrixStack.cpp
+++ MatrixStack.cpp
@@ -29,7 +29,7 @@ void MatrixStack::push( const Matrix4f& m )
 	// Push m onto the stack.
 	// Your stack should have OpenGL semantics:
 	// the new top should be the old top multiplied by m
-    Matrix4f product = m * this->top();
+    Matrix4f product =  this->top() * m;
     m_matrices.push_back(product);
 }
 
